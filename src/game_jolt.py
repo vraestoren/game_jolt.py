00a6dcf89ddf0a6b27d1dacd38fab520cf08949a
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-14 12:44:35 +0300

    Update game_jolt.py

--- src/game_jolt.py
+++ src/game_jolt.py
@@ -1,13 +1,9 @@
 from hashlib import md5
 from requests import Session
+from urllib.parse import urlencode
 
 class GameJolt:
-	def __init__(
-			self,
-			game_id: int,
-			username: str,
-			user_token: str,
-			private_key: str) -> None:
+	def __init__(self, game_id: int, username: str, user_token: str, private_key: str) -> None:
 		self.api = "https://api.gamejolt.com/api/game/v1_2"
 		self.session = Session()
 		self.session.headers = {
@@ -18,66 +14,71 @@ class GameJolt:
 		self.user_token = user_token
 		self.private_key = private_key
 
-	def generate_signature(self, path: str) -> str:
-		return md5(
-			f"{self.api}{path}{self.private_key}".encode()).hexdigest()
-	
+	def _get(
+			self, endpoint: str, params: dict = None, include_user: bool = True) -> dict:
+		payload = {
+			"game_id": self.game_id
+		}
+		if include_user and not (params and params.get("guest")):
+			payload.update({
+				"username": self.username,
+				"user_token": self.user_token
+			})
+		
+		if params:
+			payload.update({
+				key: value for key, value in params.items() if value is not None
+			})
+		
+		query = urlencode(payload)
+		path = f"{endpoint}?{query}" if query else endpoint
+		signature = md5(f"{self.api}{path}{self.private_key}".encode()).hexdigest()
+		return self.session.get(
+			f"{self.api}{path}&signature={signature}").json()
+
 	def get_user(self) -> dict:
-		path = f"/users/?game_id={self.game_id}&username={self.username}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-			
+		params = {
+			"username": self.username
+		}
+		return self._get("/users/", params)
+
 	def authenticate_user(self) -> dict:
-		path = f"/users/auth/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		return self._get("/users/auth/")
+
 	def open_session(self) -> dict:
-		path = f"/sessions/open/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
+		return self._get("/sessions/open/")
 
 	def ping_session(self, status: str = "active") -> dict:
-		path = f"/sessions/ping/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}&status={status}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		params = {
+			"status": status
+		}
+		return self._get("/sessions/ping/", params)
+
 	def check_session(self) -> dict:
-		path = f"/sessions/check/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		return self._get("/sessions/check/")
+
 	def close_session(self) -> dict:
-		path = f"/sessions/close/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		return self._get("/sessions/close/")
+
 	def get_scores(
-			self, 
+			self,
 			limit: int = None,
 			table_id: int = None,
 			guest: str = None,
 			better_than: int = None,
 			worse_than: int = None) -> dict:
-		path = f"/scores/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}"
-		if limit:
-			path += f"&limit={limit}"
-		if table_id:
-			path += f"&table_id={table_id}"
-		if guest:
-			path = f"/scores/?game_id={self.game_id}&guest={guest}"
-		if better_than:
-			path += f"&better_than={better_than}"
-		if worse_than:
-			path += f"&worse_than={worse_than}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		params = {
+			"limit": limit,
+			"table_id": table_id,
+			"guest": guest,
+			"better_than": better_than,
+			"worse_than": worse_than,
+		}
+		return self._get("/scores/", params)
+
 	def get_score_tables(self) -> dict:
-		path = f"/scores/tables/?game_id={self.game_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		return self._get("/scores/tables/")
+
 	def add_scores(
 			self,
 			score: str,
@@ -85,95 +86,74 @@ class GameJolt:
 			table_id: int = None,
 			extra_data: str = None,
 			guest: str = None) -> dict:
-		path = f"/scores/add/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}&score={score}&sort={sort}"
-		if table_id:
-			path += f"&table_id={table_id}"
-		elif extra_data:
-			path += f"&extra_data={extra_data}"
-		elif guest:
-			path = f"/scores/add/?game_id={self.game_id}&score={score}&sort={sort}&guest={guest}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
+		params = {
+			"score": score,
+			"sort": sort,
+			"table_id": table_id,
+			"extra_data": extra_data,
+			"guest": guest
+		}
+		return self._get("/scores/add/", params)
 
-	def get_score_rank(
-			self,
-			sort: int,
-			table_id: int = None) -> dict:
-		path = f"/scores/get-rank/?game_id={self.game_id}&sort={sort}"
-		if table_id:
-			path += f"&table_id={table_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
-	def get_trophy(
-			self,
-			trophy_id: int = None,
-			achieved: bool = False) -> dict:
-		path = f"/trophies/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}&achieved={achieved}"
-		if trophy_id:
-			path += f"&trophy_id={trophy_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+	def get_score_rank(self, sort: int, table_id: int = None) -> dict:
+		params = {
+			"sort": sort,
+			"table_id": table_id
+		}
+		return self._get("/scores/get-rank/", params)
+
+	def get_trophy(self, trophy_id: int = None, achieved: bool = False) -> dict:
+		params = {
+			"trophy_id": trophy_id,
+			"achieved": str(achieved).lower()
+		}
+		return self._get("/trophies/", params)
+
 	def add_achieved(self, trophy_id: int) -> dict:
-		path = f"/trophies/add-achieved/?game_id={game_id}&username={self.username}&user_token={self.user_token}&trophy_id={trophy_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		params = {
+			"trophy_id": trophy_id
+		}
+		return self._get("/trophies/add-achieved/", params)
+
 	def remove_achieved(self, trophy_id: int) -> dict:
-		path = f"/trophies/remove-achieved/?game_id={game_id}&username={self.username}&user_token={self.user_token}&trophy_id={trophy_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		params = {
+			"trophy_id": trophy_id
+		}
+		return self._get("/trophies/remove-achieved/", params)
+
 	def set_data(
-			self,
-			key: str,
-			data: str,
-			user_info_only: bool = False) -> dict:
-		path = f"/data-store/set/?game_id={self.game_id}&key={key}&data={data}"
-		if user_info_only:
-			path += f"&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
-	def update_data(
-			self,
-			key: str,
-			operation: str,
-			value: int,
-			user_info_only: bool = False) -> dict:
-		path = f"/data-store/update/?game_id={self.game_id}&key={key}&operation={operation}&value={value}"
-		if user_info_only:
-			path += f"&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
-	def remove_data(self, key: str) -> dict:
-		path = f"/data-store/remove/?game_id={self.game_id}&key={key}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-		
-	def get_data(
-			self,
-			key: str,
-			user_info_only: bool = False) -> dict:
-		path = f"/data-store/?game_id={self.game_id}&key={key}"
-		if user_info_only:
-			path += f"&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
-	def get_keys(self) -> dict:
-		path = f"/data-store/get-keys/?game_id={self.game_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+			self, key: str, data: str, user_info_only: bool = False) -> dict:
+		params = {
+			"key": key,
+			"data": data
+		}
+		return self._get("/data-store/set/", params, user_info_only)
+
+	def update_data(self, key: str, operation: str, value: int, user_info_only: bool = False) -> dict:
+		params = {
+			"key": key,
+			"operation": operation,
+			"value": value
+		}
+		return self._get("/data-store/update/", params, user_info_only)
+
+	def remove_data(self, key: str, user_info_only: bool = False) -> dict:
+		params = {
+			"key": key
+		}
+		return self._get("/data-store/remove/", params, user_info_only)
+
+	def get_data(self, key: str, user_info_only: bool = False) -> dict:
+		params = {
+			"key": key
+		}
+		return self._get("/data-store/", params, user_info_only)
+
+	def get_keys(self, user_info_only: bool = False) -> dict:
+		return self._get("/data-store/get-keys/", {}, user_info_only)
+
 	def get_friends_list(self) -> dict:
-		path = f"/friends/?game_id={self.game_id}&username={self.username}&user_token={self.user_token}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
-	
+		return self._get("/friends/")
+
 	def get_server_time(self) -> dict:
-		path = f"/time/?game_id={self.game_id}"
-		return self.session.get(
-			f"{self.api}{path}&signature={self.generate_signature(path)}").json()
+		return self._get("/time/")
